5b9193ea57af4ccf09fba79b2d351b3ecc8d84e2
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-05-18 13:12:16 +0545

    init: basic folder structure created

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-pink-200 font-bold text-center text-2xl'>App</div>
+  ) 
+}
+
+export default App
